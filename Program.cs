7c32bf180576ef54d6e1debd040274155e43696d
Peter Mhlanga <mhlpet015@myuct.ac.za>
2021-12-05 16:45:00 +0200

    completed producing the occurance string

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace myproject
 {
@@ -23,7 +24,9 @@ namespace myproject
         static void calculateMatch(string firstname, string secondname)
         {
             //create temp string
-            string temporary = firstname;
+            string temp = firstname;
+            string temporary = String.Concat(temp.Where(c => !Char.IsWhiteSpace(c)));
+            string occuranceString = "";
 
 
 
@@ -31,9 +34,12 @@ namespace myproject
             while (temporary.Length > 0)
             {
 
+                //count occurance of each char
                 char ch = temporary[0];
                 int frequency = temporary.Count(f => (f == ch));
                 Console.WriteLine("frequency of:" + temporary[0] + " is " + frequency);
+                occuranceString = occuranceString + frequency;
+                Console.WriteLine("OccuranceString: " + occuranceString);
 
                 //remove current indice
                 temporary = temporary.Replace(temporary[0].ToString(), String.Empty);
