451a712f3ba2be36c9b4519ce852e70438a72522
Peter Mhlanga <mhlpet015@myuct.ac.za>
2021-12-05 14:17:38 +0200

    completed identifying indices to be removed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace myproject
 {
@@ -9,18 +10,60 @@ namespace myproject
         static void Main(string[] args)
         {
 
-            string fname, lname;
+            string fname = "";
+            string lname = "";
             Console.Write("Enter the first name: ");
             fname = Console.ReadLine();
-            Console.Write("Enter the second name: ");
-            lname = Console.ReadLine();
+            //Console.Write("Enter the second name: ");
+            //lname = Console.ReadLine();
             calculateMatch(fname, lname);
 
         }
 
         static void calculateMatch(string firstname, string secondname)
         {
-            Console.Write(firstname + " " + secondname);
+            string temporary = firstname;
+            var indicesToBeRemoved = new ArrayList();
+
+            //iterate through the string for each char in string
+            for (int i = 0; i < firstname.Length - 1; i++)
+            {
+                //for each char iterate through all the other chars in the word
+                for (int j = i + 1; j < firstname.Length; j++)
+                {
+                    //if char matches our char
+                    if (firstname[i] == firstname[j])
+                    {
+                        //add char indice to array
+                        indicesToBeRemoved.Add(j);
+                    }
+                }
+                //show all char indices to be removed
+                Console.WriteLine("for " + firstname[i] + "we remove: ");
+                PrintValues(indicesToBeRemoved);
+                indicesToBeRemoved.Clear();
+            }
         }
+        //remove all similar chars at their indices
+        //for (int k = 0; k < indicesToBeRemoved.size(); k++)
+        // {
+        // temporary = temporary.Replace(indicesToBeRemoved[i].ToString(), String.Empty);
+        // }
+        // Console.WriteLine(temporary);
+        //}
+        // }
+
+
+        public static void PrintValues(IEnumerable myList)
+        {
+            foreach (Object obj in myList)
+                Console.Write("   {0}", obj);
+            Console.WriteLine();
+        }
+
+
+
+
+
     }
 }
