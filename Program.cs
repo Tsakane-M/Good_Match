0bfa5abaf97d9e548ea9165a5d14c2739e1fca9b
Peter Mhlanga <mhlpet015@myuct.ac.za>
2021-12-05 12:46:07 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace myproject
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string fname, lname;
+            Console.Write("Enter your firstname: ");
+            fname = Console.ReadLine();
+            Console.Write("Enter your lastname: ");
+            lname = Console.ReadLine();
+            Console.Write("Your fullname is " + fname + " " + lname);
+
+        }
+    }
+}
