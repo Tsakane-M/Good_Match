505c82cc7e59fa43af8c317a9a9de5e2d566b449
Peter Mhlanga <mhlpet015@myuct.ac.za>
2021-12-05 18:46:45 +0200

    completed obtaining sum string

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ namespace myproject
             string temp = firstname;
             string temporary = String.Concat(temp.Where(c => !Char.IsWhiteSpace(c)));
             string occuranceString = "";
+            string sumString = "";
 
 
 
@@ -37,25 +38,54 @@ namespace myproject
                 //count occurance of each char
                 char ch = temporary[0];
                 int frequency = temporary.Count(f => (f == ch));
-                Console.WriteLine("frequency of:" + temporary[0] + " is " + frequency);
+                //Console.WriteLine("frequency of:" + temporary[0] + " is " + frequency);
                 occuranceString = occuranceString + frequency;
-                Console.WriteLine("OccuranceString: " + occuranceString);
+
 
                 //remove current indice
                 temporary = temporary.Replace(temporary[0].ToString(), String.Empty);
 
                 //print new word
-                Console.WriteLine("new string is :" + temporary);
-                Console.WriteLine();
+                //Console.WriteLine("new string is :" + temporary);
+                //Console.WriteLine();
+
 
             }
+            Console.WriteLine("OccuranceString: " + occuranceString);
+            //use occurance string to calculate 2 digit number
+            while (occuranceString.Length > 1)
+            {
+
+                int sum = 0;
+                int left = (int)Char.GetNumericValue(occuranceString[0]);
+                int right = (int)Char.GetNumericValue(occuranceString[occuranceString.Length - 1]);
+                sum = left + right;
+                sumString = sumString + "" + sum.ToString();
+                occuranceString = occuranceString.Remove(0, 1);
+                occuranceString = occuranceString.Remove((occuranceString.Length) - 1);
+                //Console.WriteLine(sum);
+                //Console.WriteLine("sum string string is " + sumString);
+                //Console.WriteLine();
+                //Console.WriteLine("new string is " + occuranceString);
+
+                if (occuranceString.Length == 1)
+                {
+                    sumString = sumString + "" + occuranceString;
+
+                }
+            }
+            Console.WriteLine("sum string string is " + sumString);
 
         }
 
 
+
     }
 
+
 }
+
+
 
 
 
